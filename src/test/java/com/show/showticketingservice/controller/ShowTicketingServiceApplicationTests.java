48573cb05fad38dd541189a320a4697d40d19f44
Sin Hak <tls7gkr@naver.com>
2021-06-01 16:04:55 +0900

    g

--- src/test/java/com/show/showticketingservice/controller/ShowTicketingServiceApplicationTests.java
+++ src/test/java/com/show/showticketingservice/controller/ShowTicketingServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.show.showticketingservice;
+package com.show.showticketingservice.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
